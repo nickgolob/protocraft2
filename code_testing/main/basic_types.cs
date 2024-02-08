1184a88859928ab302b509ea011296e13150b611
nickgolob <nickgolob@gmail.com>
2024-02-07 20:47:24 -0800

    shield

--- code_testing/main/basic_types.cs
+++
@@ -1,9 +0,0 @@
-namespace main;
-
-// arms length = 3 ft
-using DistanceFeet = double;
-// short sword blade = 5 lbs. Battle axe weight = 20 lbs?
-using WeightPounds = double;
-
-// wood == 1.0. Steel = 2.0?
-using Hardness = double;
